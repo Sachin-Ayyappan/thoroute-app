ab1ef29a836c3c4ddb43585772a52c81510bf680
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-06-27 22:04:01 +0530

    passverification

---
+++ scripts/verify.js
@@ -0,0 +1,13 @@
+function verifyPassword() {  
+    var pw = document.getElementById("pswd").value;  
+    var us = document.getElementById("usnm").value; 
+    if(pw == "password" && us=="admin") {
+        alert("Vro, y this no redir?");
+    }  
+    else{
+        alert("Not so success vro!! :(");
+    }
+}
+function redir(){
+    window.location.href("https://www.google.com")
+}
