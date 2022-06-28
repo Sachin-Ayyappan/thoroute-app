cf327870c137c5a662f39ba3a53e87bcddd4c0c2
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-06-28 22:25:33 +0530

    cleanup

--- scripts/verify.js
+++ scripts/verify.js
@@ -2,6 +2,7 @@ function verifyPassword() {
     var pw = document.getElementById("pswd").value;  
     var us = document.getElementById("usnm").value; 
     if(pw == "password" && us=="admin") {
+        window.location.href = "../index.html";
         alert("Vro, y this no redir?");
     }  
     else{
