4c0c4e893e6e2bfc0d363797e33df54c544754dc
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-07-02 11:45:05 +0530

    kgl

--- assets/scripts/verify.js
+++ assets/scripts/verify.js
@@ -1,8 +1,8 @@
 function verifyPassword() {  
     var pw = document.getElementById("pswd").value;  
     var us = document.getElementById("usnm").value; 
-    if(pw == "password" && us=="admin") {
-        window.location.href = "../index.html";
+    if(pw=="password" && us=="admin") {
+        window.location.href="../index.html";
         alert("Vro, y this no redir?");
     }  
     else{
