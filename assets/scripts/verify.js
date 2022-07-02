1c178eee1c989698d43dfa0b1417f92799f42425
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-07-03 00:52:02 +0530

    Dashboard added and modified

--- assets/scripts/verify.js
+++ assets/scripts/verify.js
@@ -2,7 +2,7 @@ function verifyPassword() {
     var pw = document.getElementById("pswd").value;  
     var us = document.getElementById("usnm").value; 
     if(pw=="password" && us=="admin") {
-        window.location.href="../index.html";
+        window.location.href="../assets/dashboard/index.html";
         alert("Vro, y this no redir?");
     }  
     else{
