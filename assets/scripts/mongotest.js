934d9a219c4de2a2dc95ca6db7c06b9cec204f8f
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-07-06 14:25:49 +0530

    hello

---
+++ assets/scripts/mongotest.js
@@ -0,0 +1,22 @@
+var MongoClient = require("mongodb").MongoClient;
+var url =
+  "mongodb+srv://thoroute-app:thoroute%402022adi@thoroute.vd7gshc.mongodb.net/?retryWrites=true&w=majority";
+us = "jadithyan";
+pass="teeemp";
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("users");
+  var query = { username: us };
+  dbo
+    .collection("users")
+    .find(query)
+    .toArray(function (err, result) {
+      if (err) throw err;
+      pass = result[0].password;
+      console.log(pass);
+      console.log(result[0]);
+      db.close();
+      return pass;
+    });
+});
+console.log(pass);
