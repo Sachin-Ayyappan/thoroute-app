7e2d796f364ec115259b0fdcc5bac0ec18624b60
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-09-22 00:15:36 +0530

    imdumb

--- assets/scripts/verify.js
+++ assets/scripts/verify.js
@@ -3,8 +3,9 @@ var attempt = 3; // Variable to count number of attempts.
 function verifyPassword() {
   var username = document.getElementById("exampleInputUsername").value;
   var password = document.getElementById("exampleInputPassword").value;
-  if (username == "thoroute" && password == "admin123") {s
-    window.location = "../../index.html"; // Redirecting to other page.
+  if (username == "thoroute" && password == "admin123") {
+    location.assign("http://localhost:5500/assets/dashboard/index.html"); 
+    console.log("Working!!!");  // Redirecting to other page.
     return false;
   } else {
     attempt--; // Decrementing by one.
